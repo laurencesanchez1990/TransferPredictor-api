a1b866bcd2070b45452d723869dc85ecdd3a247f
Laurence Philpott <philpott_1990@hotmail.co.uk>
2018-01-15 20:47:20 +0000

    multiple player recommendation

--- server.js
+++ server.js
@@ -15,17 +15,35 @@ const lowestScoringPlayer = () => {
 	return sortedTeam[0]
 }
 
+const getLowestScoringPlayers = () => {
+	const team = getTeam();
+	const sortedTeam = _.sortBy(team, "total_points");
+	return sortedTeam.slice(0,3)
+}
+
 const getNewPlayer = (playerValue, playerPos) => {
-	const newPlayers = _.filter(list, (player) => {return (player.element_type === playerPos) && (player.now_cost <= playerValue); });
-	console.log(newPlayers)
+	const newPlayers = []
+	_.each(list, (player) => {
+		if ((player.element_type === playerPos) && (player.now_cost <= playerValue)){
+			newPlayers.push( {
+				name: player.web_name,
+				cost: player.now_cost,
+			})
+		};
+		 });
 	const transfer = _.sortBy(newPlayers, "total_points");
-	return transfer.reverse()
+	return transfer.reverse().slice(0, 3)
 }
 
 
 app.get('/api/recommendations', (req, res) => {
-	const lowestScoringPlayer = lowestScoringPlayer() 
-	const newPlayers = getNewPlayer(lowestScoringPlayer.now_cost, lowestScoringPlayer.element_type)
-	res.json({playersFound: newPlayers.length, playerCost: lowestScoringPlayer.now_cost, playerPos: lowestScoringPlayer.element_type, data: newPlayers})
+	const lowestScoring = getLowestScoringPlayers() 
+	const foundPlayers = lowestScoring.map((foundPlayer) => {
+		return {
+			name: foundPlayer.web_name,
+			cost: foundPlayer.now_cost,
+			recommendations: getNewPlayer(foundPlayer.now_cost, foundPlayer.element_type)
+		}})
+	res.json({playersFound: foundPlayers.length, data: foundPlayers})
 })
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
